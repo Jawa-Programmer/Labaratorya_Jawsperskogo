febcd48ad96999e3de4021e7fdb3a0ea3a159bbf
Jawa-Programmer <jawaprogrammer@gmail.com>
2020-05-17 19:55:09 +0300

    Вроде бы как шестая лаба готова)

--- lab6a/MyGraph.cpp
+++ lab6a/MyGraph.cpp
@@ -1,17 +1,57 @@
 #include "MyGraph.h"
+#include <iostream>
+
+
+void MyNodesList::clear()
+{
+	MyNodesList* tmp1 = next;
+	delete info;
+	while (tmp1)
+	{
+		MyNodesList* tmp = tmp1->next;
+		delete tmp1->info;
+		delete tmp1;
+		tmp1 = tmp;
+	}
+}
+
+void MyNodesList::clean()
+{
+	MyNodesList* tmp1 = next;
+	while (tmp1)
+	{
+		MyNodesList* tmp = tmp1->next;
+		delete tmp1;
+		tmp1 = tmp;
+	}
+}
+
+void MyNodesList::print()
+{
+	MyNodesList* tmp = this;
+	std::cout << "{";
+	while (tmp)
+	{
+		std::cout << "(" << tmp->info->x << "," << tmp->info->y << "), ";
+		tmp = tmp->next;
+	}
+	std::cout << "}\n";
+}
 
 MyGraphNode* MyNodesList::find(int x, int y)
 {
 	MyNodesList* tmp = this;
-	while (tmp->next != nullptr)
+	while (tmp != nullptr)
 	{
 		if (tmp->info->x == x && tmp->info->y == y)return tmp->info;
+		tmp = tmp->next;
 	}
 	return nullptr;
 }
 MyGraphErrors MyNodesList::add(MyGraphNode* toAdd)
 {
 	MyNodesList* tmp = this;
+	if (*tmp->info == *toAdd)return MG_COORDS_BUSY;
 	while (tmp->next != nullptr)
 	{
 		if (*tmp->info == *toAdd)return MG_COORDS_BUSY;
@@ -49,6 +89,11 @@ MyGraphErrors MyNodesList::remove(MyGraphNode* toDel)
 	return MG_COORDS_NOT_FOUNDED;
 }
 
+MyGraphNode::~MyGraphNode()
+{
+	//	clearArcs();
+}
+
 void MyGraphNode::addIncident(MyGraphNode* toAdd)
 {
 	if (incidents->find(toAdd->x, toAdd->y))return;
@@ -66,6 +111,15 @@ void MyGraphNode::removeIncident(MyGraphNode* toRemove)
 		if (incidents->info == toRemove) { MyNodesList* tmp = incidents; incidents = incidents->next; delete tmp; }
 		else incidents->remove(toRemove);
 	toRemove->removeIncoming(this);
+}
+
+void MyGraphNode::clearArcs()
+{
+	while (incoming)
+	{
+		incoming->info->removeIncident(this);
+	}
+	while (incidents)removeIncident(incidents->info);
 }
 
 void MyGraphNode::addIncoming(MyGraphNode* toAdd)
@@ -98,6 +152,13 @@ bool MyGraphNode::operator==(MyGraphNode b)
 	return x == b.x && y == b.y;
 }
 
+MyGraph::~MyGraph()
+{
+	nodes->clear();
+	delete nodes;
+	nodes = nullptr;
+}
+
 MyGraphErrors MyGraph::addNode(int x, int y)
 {
 	if (nodes) {
@@ -114,6 +175,30 @@ MyGraphErrors MyGraph::addNode(int x, int y)
 		nodes->info->y = y;
 	}
 	return MG_NO_ERROR;
+}
+
+MyGraphErrors MyGraph::removeNode(int x, int y)
+{
+	if (nodes) {
+		if (nodes->info->x == x && nodes->info->y == y)
+		{
+			MyNodesList* tmp = nodes->next;
+			nodes->info->clearArcs();
+			delete nodes;
+			nodes = tmp;
+			return MG_NO_ERROR;
+		}
+		else
+		{
+			MyGraphNode* toDel = nodes->find(x, y);
+			if (toDel)
+			{
+				toDel->clearArcs();
+				return nodes->remove(toDel);
+			}
+		}
+	}
+	return MG_COORDS_NOT_FOUNDED;
 }
 
 MyGraphErrors MyGraph::addArc(int x1, int y1, int x2, int y2)
@@ -136,4 +221,158 @@ MyGraphErrors MyGraph::removeArc(int x1, int y1, int x2, int y2)
 		return MG_NO_ERROR;
 	}
 	return MG_COORDS_NOT_FOUNDED;
+}
+
+void MyGraph::printToConsole()
+{
+	{
+		MyNodesList* tmp = nodes;
+		while (tmp)
+		{
+			std::cout << "\t" << tmp->info->x;
+			tmp = tmp->next;
+		}
+		std::cout << "\n";
+		tmp = nodes;
+		while (tmp)
+		{
+			std::cout << "\t" << tmp->info->y;
+			tmp = tmp->next;
+		}
+		std::cout << "\n";
+		tmp = nodes;
+		while (tmp)
+		{
+			std::cout << tmp->info->x << " " << tmp->info->y;
+			{
+				MyNodesList* tmp2 = nodes;
+				while (tmp2)
+				{
+					if (tmp->info->incoming->find(tmp2->info->x, tmp2->info->y))std::cout << "\t+";
+					else std::cout << "\t-";
+					tmp2 = tmp2->next;
+				}
+			}
+			std::cout << "\n";
+			tmp = tmp->next;
+		}
+	}
+
+}
+
+void MyGraph::cleanN()
+{
+	timer = 0;
+	MyNodesList* tmp = nodes;
+	while (tmp)
+	{
+		tmp->info->N = -1;
+		tmp->info->Visited = false;
+		tmp->info->forvis = false;
+		tmp = tmp->next;
+	}
+}
+
+void MyGraph::DFB()
+{
+	MyNodesList* tmp = nodes;
+	while (tmp)
+	{
+		if (tmp->info->N == -1)DFBr(tmp->info);
+		tmp = tmp->next;
+	}
+}
+
+void MyGraph::DFBr(MyGraphNode* node)
+{
+	timer++;
+	node->forvis = true;
+	MyNodesList* tmp = node->incoming;
+	while (tmp)
+	{
+		if (!tmp->info->forvis)DFBr(tmp->info);
+		tmp = tmp->next;
+	}
+	node->N = timer;
+	timer++;
+}
+
+void MyGraph::iDFB(MyGraphNode* node, MyNodesList* graph)
+{
+	node->Visited = true;
+	graph->add(node);
+	MyNodesList* tmp = sort(node->incidents);
+	while (tmp)
+	{
+		if (!tmp->info->Visited)iDFB(tmp->info, graph);
+		tmp = tmp->next;
+	}
+}
+
+MyNodesList* MyGraph::sort(MyNodesList* mndl)
+{
+	if (!mndl)return nullptr;
+
+	MyGraphNode* minG = nullptr;
+
+	MyNodesList* tmp = mndl, * uns = nullptr, * unsEnd = nullptr;
+	int max = tmp->info->N;
+	minG = tmp->info;
+	while (tmp)
+	{
+		if (tmp->info->N > max) { max = tmp->info->N; minG = tmp->info; }
+		tmp = tmp->next;
+	}
+
+	uns = new MyNodesList;
+	uns->info = minG;
+	unsEnd = uns;
+	while (1)
+	{
+		int prevMax = max;
+		max = -1;
+		tmp = mndl;
+		while (tmp)
+		{
+			if (tmp->info->N < prevMax && tmp->info->N > max ) { max = tmp->info->N; minG = tmp->info; }
+			tmp = tmp->next;
+		}
+		if (max == -1)break;
+		unsEnd->next = new MyNodesList();
+		unsEnd = unsEnd->next;
+		unsEnd->info = minG;
+	}
+	return uns;
+}
+
+MyList<MyNodesList>* MyGraph::StronglyConnected()
+{
+	cleanN();
+	DFB();
+	MyNodesList* sorted = sort(nodes), * tmp = sorted;
+	MyList<MyNodesList>* uns = nullptr, * unsHead = nullptr;
+	while (tmp)
+	{
+		if (!tmp->info->Visited) {
+			MyNodesList* pr = new MyNodesList;
+			pr->info = tmp->info;
+			iDFB(tmp->info, pr);
+
+			if (uns) {
+				uns->next = new MyList<MyNodesList>;
+				uns = uns->next;
+				uns->info = pr;
+			}
+			else
+			{
+				uns = new MyList<MyNodesList>;
+				uns->info = pr;
+				unsHead = uns;
+			}
+		}
+		tmp = tmp->next;
+	}
+	sorted->clean();
+	delete sorted;
+	return unsHead;
 }
--- lab6a/lab6a.cpp
+++ lab6a/lab6a.cpp
@@ -1,20 +1,52 @@
-﻿// lab6a.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "MyGraph.h"
+using namespace std;
 
-#include <iostream>
+///Функция безопасного чтения из консоли
+template <typename T> void safeGet(T& variable)
+{
+	cin >> variable;
+	while (!cin)
+	{
+		cin.clear();
+		cin.ignore(cin.rdbuf()->in_avail());
+		cout << "incorrect input, try again: ";
+		cin >> variable;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	system("color 1E");
+	system("mode con cols=160 lines=30");
+	MyGraph graph;
+	graph.addNode(1, 1);
+	graph.addNode(1, 2);
+	graph.addNode(1, 3);
+
+	graph.addNode(2, 1);
+	graph.addNode(2, 2);
+	graph.addNode(2, 3);
+
+	graph.addArc(1, 1, 1, 2);
+	graph.addArc(1, 2, 1, 1);
+	graph.addArc(1, 2, 2, 1);
+
+	graph.addArc(1, 3, 2, 1);
+	graph.addArc(2, 1, 2, 2);
+	graph.addArc(2, 2, 2, 3);
+	graph.addArc(2, 3, 1, 3);
+
+	graph.printToConsole();
+	cout << "----------------------------------------------------" << endl;
+	{
+		MyList<MyNodesList>* sc = graph.StronglyConnected(), * sct = sc;
+		while (sct) {
+			sct->info->print();
+			sct = sct->next;
+		}
+	}
+	cout << "----------------------------------------------------" << endl;
+	graph.removeNode(1, 1);
+	graph.printToConsole();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
